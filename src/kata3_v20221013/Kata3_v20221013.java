fc837aff3cb44bdcec4911fd8dee30341fa1d589
Alejandro Vialard <alejandrovialard2002@gmail.com>
2022-10-13 09:24:28 -0100

    Versión 1 de Kata3

---
+++ src/kata3_v20221013/Kata3_v20221013.java
@@ -0,0 +1,10 @@
+package kata3_v20221013;
+
+public class Kata3_v20221013 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histograma display...");
+        histogramDisplay.execute();
+    }
+    
+}
